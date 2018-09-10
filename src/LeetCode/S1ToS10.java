b4c1e9f9b62de2485e79ff3c36655e4376f4f91f
tkanng <tkanng@gmail.com>
2018-09-10 23:10:39 +0800

    update

--- src/LeetCode/S1ToS10.java
+++ src/LeetCode/S1ToS10.java
@@ -1,4 +1,3 @@
-import java.sql.Statement;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -7,7 +6,14 @@ import java.util.Set;
 public class S1ToS10 {
 
     public static void main(String[] args) {
-
+//        int rollMeOver = Integer.MAX_VALUE + 1;
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(rollMeOver);
+//        System.out.println(Integer.MAX_VALUE + 1 > Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MIN_VALUE - 1);
+        System.out.println(myAtoi("2147483648"));
+        System.out.println(Integer.MIN_VALUE);
     }
 
     //s3:Longest Substring Without Repeating Characters
@@ -27,6 +33,7 @@ public class S1ToS10 {
         return ans;
     }
 
+    //S3
     public int lengthOfLongestSubstring2(String s) {
         int n = s.length(), ans = 0;
         Map<Character, Integer> map = new HashMap<>(); // current index of character
@@ -41,13 +48,14 @@ public class S1ToS10 {
         return ans;
     }
 
-
-    // S4
+    // S4 不会
 //    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 //        int m = nums1.length;
 //        int n = nums2.length;
 //
 //    }
+
+
     // S5 Dynamic Programming
     public String longestPalindrome(String s) {
         int n = s.length();
@@ -55,7 +63,13 @@ public class S1ToS10 {
         boolean[][] dp = new boolean[n][n];
         for (int i = n - 1; i >= 0; i--) {
             for (int j = i; j < n; j++) {
-                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+                if (i == j)
+                    dp[i][j] = true;
+                else if (i == j - 1)
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                else {
+                    dp[i][j] = s.charAt(i) == s.charAt(j) && (dp[i + 1][j - 1]);
+                }
                 if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
                     res = s.substring(i, j + 1);
                 }
@@ -63,6 +77,101 @@ public class S1ToS10 {
         }
         return res;
     }
+
+
+    // S6 不会
+
+
+    // S7 reverse integer.  检查整数溢出情况！
+
+    public static int reverse(int x) {
+        int result = 0;
+        int dig = 0;
+        int temp = 0;
+        while (x != 0) {
+            dig = x % 10;
+            temp = dig + result * 10; // 正数溢出，会变为负数;负数溢出，会变为正数
+            if ((temp - dig) / 10 != result) {
+                return 0;
+            }
+            result = temp;
+            x = x / 10;
+        }
+        return result;
+    }
+
+    // S8: String to Integer (atoi)
+
+    public static int myAtoi(String str) {
+        int len = str.length();
+        int i = 0;
+        int flag = 1;
+        long tmp = 0l, result = 0l;
+        for (i = 0; i < len; ++i) {
+            if (str.charAt(i) != ' ')
+                break;
+        }
+        if (i == len) return 0;
+        if (str.charAt(i) == '-') {
+            flag = -1;
+            ++i;
+        } else if (str.charAt(i) == '+') {
+            ++i;
+        } else if ((str.charAt(i) < '0') || (str.charAt(i) > '9')) return 0;
+
+
+        if (flag == 1) {
+            for (; i < str.length(); ++i) {
+                if ((str.charAt(i) < '0') || (str.charAt(i) > '9')) break;
+                result = tmp * 10 + (str.charAt(i) - '0');
+
+                if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+                tmp = result;
+            }
+        } else {
+            for (; i < str.length(); ++i) {
+                if ((str.charAt(i) < '0') || (str.charAt(i) > '9')) break;
+                result = tmp * 10 - (str.charAt(i) - '0');
+                if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+                tmp = result;
+            }
+        }
+        return (int) result;
+    }
+
+
+    // S9
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        String s = String.valueOf(x);
+        int len = s.length();
+        int low = 0;
+        int high = s.length() - 1;
+        if (len % 2 == 0) {
+            //even
+            for (int i = 0; i < len / 2; ++i) {
+                if (s.charAt(low) != s.charAt(high))
+                    return false;
+                ++low;
+                --high;
+            }
+            return true;
+        } else {
+            //odd
+            for (int i = 0; i < len / 2; ++i) {
+                if (s.charAt(low) != s.charAt(high))
+                    return false;
+                ++low;
+                --high;
+            }
+            return true;
+        }
+    }
+
+
+
+
+
 
 
 
